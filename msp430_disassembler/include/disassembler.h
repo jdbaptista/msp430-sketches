0f94f53ad14532b68c787f8dba4f39e7722254bc
Jaden Baptista <baptistajaden@berkeley.edu>
2023-07-07 13:50:46 -0700

    remove blinky and add disassembler

---
+++ msp430_disassembler/include/disassembler.h
@@ -0,0 +1,114 @@
+/*
+ * disassembler.h
+ *
+ *  Created on: Jul 5, 2023
+ *      Author: Jaden Baptista
+ */
+
+#ifndef INCLUDE_DISASSEMBLER_H_
+#define INCLUDE_DISASSEMBLER_H_
+
+#include <stdint.h>
+#include "types.h"
+#include "masks.h"
+
+/**
+ * It is the responsibility of the programmer to first learn the register addressing modes of
+ * the source and destination operand. Then, they must find the correct offset in the next two
+ * words and format them with the formatOffset function. If there is no offset for either operand,
+ * then they should be set to NULL to avoid errors.
+ *
+ * NOTE: For single operand instructions, DestOffset should be used instead of source offset!
+ */
+void getAsm(char** result, uint16_t byteCode, opCode* code, char* srcOffset, char* destOffset);
+
+/**
+ * It is the responsibility of the programmer to choose the correct function.
+ * This choice should be based on getDestRegisterMode and getSourceRegisterMode.
+ */
+void getAsmSingleOffset(char** result, uint16_t byteCode, opCode* code, uint16_t srcOffset);
+
+/**
+ * It is the responsibility of the programmer to choose the correct function.
+ * This choice should be based on getDestRegisterMode and getSourceRegisterMode.
+ */
+void getAsmDoubleOffset(char** result, uint16_t byteCode, opCode* code, uint16_t srcOffset, uint16_t destOffset);
+
+opCode getOperation(uint16_t byteCode);
+
+/**
+ * Returns the addressing mode of the destination register, Ad, in Double-Operand
+ * instructions. If called on Jump instructions, this will return an ERROR result.
+ *
+ * NOTE: Although the User Guide specifies Single-Operand instructions to use Ad,
+ * this interface uses As instead as it lines up better with the size of
+ * Double-Operand As addressing mode bits.
+ */
+addressingMode getDestRegisterMode(uint16_t byteCode, formatType type);
+
+/**
+ * Returns the addressing mode of the source register, As, in Double-Operand and
+ * Single-Operand instructions. If called on Jump instructions, this will return an
+ * ERROR result.
+ */
+addressingMode getSourceRegisterMode(uint16_t byteCode, formatType type);
+
+/**
+ * Returns the value of the destination register, D-Reg, in Double-Operand instructions.
+ * If called on Jump or Single-Operand instructions, this will result in 0.
+ */
+uint16_t getDestRegister(uint16_t byteCode, formatType type);
+
+/**
+ * Returns the value of the source register, S-Reg, in Single-Operand and Double-Operand
+ * instructions. If called on Jump instructions, this will result in 0.
+ */
+uint16_t getSourceRegister(uint16_t byteCode, formatType type);
+
+/**
+ * Returns true if B/W bit is high. This is meaningless in JUMP instructions.
+ */
+bool isByteOperation(uint16_t byteCode);
+
+char* getJumpOffset(uint16_t byteCode);
+
+/**
+ * Converts 16-bit byte code to an offset formatted string. Used when addressing
+ * mode is indexed, so the next word is the offset.
+ */
+char* byteCodeToOffset(uint16_t byteCode);
+
+char* parseRegisterNum(uint16_t regNum);
+
+static const opCode CODES[] = {
+       {RETI_MASK, RETI_TYPE, "RETI"},
+       {CALL_MASK, CALL_TYPE, "CALL"},
+       {PUSH_MASK, PUSH_TYPE, "PUSH"},
+       {SXT_MASK, SXT_TYPE, "SXT"},
+       {RRA_MASK, RRA_TYPE, "RRA"},
+       {SWPB_MASK, SWPB_TYPE, "SWPB"},
+       {RRC_MASK, RRC_TYPE, "RRC"},
+       {JNE_MASK, JNE_TYPE, "JNE"},
+       {JEQ_MASK, JEQ_TYPE, "JEQ"},
+       {JNC_MASK, JNC_TYPE, "JNC"},
+       {JC_MASK, JC_TYPE, "JC"},
+       {JN_MASK, JN_TYPE, "JN"},
+       {JGE_MASK, JGE_TYPE, "JGE"},
+       {JL_MASK, JL_TYPE, "JL"},
+       {JMP_MASK, JMP_TYPE, "JMP"},
+       {MOV_MASK, MOV_TYPE, "MOV"},
+       {ADD_MASK, ADD_TYPE, "ADD"},
+       {ADDC_MASK, ADDC_TYPE, "ADDC"},
+       {SUBC_MASK, SUBC_TYPE, "SUBC"},
+       {SUB_MASK, SUB_TYPE, "SUB"},
+       {CMP_MASK, CMP_TYPE, "CMP"},
+       {DADD_MASK, DADD_TYPE, "DADD"},
+       {BIT_MASK, BIT_TYPE, "BIT"},
+       {BIC_MASK, BIC_TYPE, "BIC"},
+       {BIS_MASK, BIS_TYPE, "BIS"},
+       {XOR_MASK, XOR_TYPE, "XOR"},
+       {AND_MASK, AND_TYPE, "AND"},
+       {0xFFFF, FORMATERROR, "ERROR"} // error acts as an end point of iteration in getOpCode()
+};
+
+#endif /* INCLUDE_DISASSEMBLER_H_ */
---
+++ msp430_disassembler/src/disassembler.c
@@ -0,0 +1,183 @@
+/*
+ * disassembler.c
+ *
+ *  Created on: Jul 5, 2023
+ *      Author: Jaden Baptista
+ */
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include "include/disassembler.h"
+
+/**
+ * Does not append a space!
+ */
+void appendOperand(char** result, char* reg, addressingMode mode, char* offset) {
+    switch (mode) {
+        case REGISTER:
+            strcat(*result, reg); // result: "R15"
+            break;
+        case INDEXED:
+            if (offset == NULL) {
+                strcat(*result, "ERROR");
+            } else {
+                strcat(*result, offset);
+                strcat(*result, "(");
+                strcat(*result, reg);
+                strcat(*result, ")"); // result: "FFFFh(R15)"
+            }
+            break;
+        case INDIRECT:
+            strcpy(*result, "@");
+            strcpy(*result, reg); // result: "@R15"
+            break;
+        case AUTOINCREMENT:
+            strcpy(*result, "@");
+            strcpy(*result, reg);
+            strcpy(*result, "+"); // result: "@R15+"
+            break;
+        default:
+            strcpy(*result, "ERROR"); // ADDRESSINGERROR
+            break;
+    }
+}
+
+void getAsm(char** result, uint16_t byteCode, opCode* opCode, char* srcOffset, char* destOffset) {
+    if (opCode->format == FORMATERROR) {
+        strcpy(*result, opCode->repr);
+        return;
+    }
+    strcpy(*result, opCode->repr); // result: "MOV"
+    if (isByteOperation(byteCode)) {
+        strcat(*result, ".B "); // result: "MOV.B "
+    } else {
+        strcat(*result, " "); // result: "MOV "
+    }
+
+    switch(opCode->format) {
+        case DOUBLE: {
+            addressingMode destMode = getDestRegisterMode(byteCode, DOUBLE);
+            addressingMode srcMode = getSourceRegisterMode(byteCode, DOUBLE);
+            uint16_t destRegister = getDestRegister(byteCode, DOUBLE);
+            char dest[10];
+            sprintf(dest, "%x", destRegister);
+            uint16_t srcRegister = getSourceRegister(byteCode, DOUBLE);
+            char src[10];
+            sprintf(src, "%x", srcRegister);
+            appendOperand(result, src, srcMode, srcOffset);
+            strcat(*result, " ");
+            appendOperand(result, dest, destMode, destOffset);
+            break;
+        }
+        case SINGLE: {
+            addressingMode destMode = getDestRegisterMode(byteCode, SINGLE);
+            uint16_t destRegister = getDestRegister(byteCode, SINGLE);
+            char dest[10];
+            sprintf(dest, "%x", destRegister);
+            appendOperand(result, dest, destMode, destOffset);
+            break;
+        }
+        case JUMP: {
+            char* pcOffset = getJumpOffset(byteCode);
+            strcat(*result, pcOffset);
+            break;
+        }
+        default:
+            strcpy(*result, "ERROR");
+            return;
+    }
+}
+
+
+
+
+// NOTE: order matters here! some masks are submasks of others!
+opCode getOpCode(uint16_t byteCode) {
+    // Note: Order of opcodes is set in disassembler.h
+    //       and is made so submasks are checked late.
+    unsigned int i = 0;
+    opCode currCode;
+    while (true) {  // danger is my middle name
+        currCode = CODES[i];
+        bool doneIterating = currCode.format == FORMATERROR;
+        bool isCorrectCode = (currCode.mask & byteCode) == currCode.mask;
+        if (doneIterating || isCorrectCode) {
+            return currCode; // only exit
+        }
+        i++;
+    }
+}
+
+addressingMode getDestRegisterMode(uint16_t byteCode, formatType type) {
+    if (type != DOUBLE) {
+        return ADDRESSINGERROR;
+    }
+    switch (byteCode & 0x0080) {
+        case 0x0080:
+            return INDEXED;
+        case 0x0000:
+            return REGISTER;
+        default:
+            return ADDRESSINGERROR;
+    }
+}
+
+addressingMode getSourceRegisterMode(uint16_t byteCode, formatType type) {
+    if (type != DOUBLE && type != SINGLE) {
+        return ADDRESSINGERROR;
+    }
+    switch (byteCode & 0x0030) {
+        case 0x0030:
+            return AUTOINCREMENT;
+        case 0x0020:
+            return INDIRECT;
+        case 0x0010:
+            return INDEXED;
+        case 0x0000:
+            return REGISTER;
+        default:
+            return ADDRESSINGERROR;
+    }
+}
+
+uint16_t getDestRegister(uint16_t byteCode, formatType type) {
+    if (type != DOUBLE) {
+        return 0;
+    }
+    return (byteCode & 0x000F);
+}
+
+uint16_t getSourceRegister(uint16_t byteCode, formatType type) {
+    switch (type) {
+    case DOUBLE:
+        return (byteCode & 0x0F00);
+    case SINGLE:
+        return (byteCode & 0x000F);
+    default:
+        return 0;
+    }
+}
+
+bool isByteOperation(uint16_t byteCode) {
+    return (byteCode & 0x0040);
+}
+
+char* getJumpOffset(uint16_t byteCode) {
+    int16_t signedOffset = (int16_t) (byteCode & 0x03FF);
+    // TODO: Implement function
+    return NULL;
+}
+
+char* byteCodeToOffset(uint16_t byteCode) {
+    // TODO: Implement function
+    int16_t signedOffset = (int16_t) byteCode;
+    return NULL;
+}
+
+char* parseRegisterNum(uint16_t regNum) {
+    // TODO: Implement function
+    return NULL;
+}
